ca737f2dfd9a2bdfcd59356139ed6cbb343a0f6d
Aallyn <support@aallyn.xyz>
2025-11-24 12:03:28 +0000

    Handle working directory for frozen applications
    Adjust current working directory for frozen executables.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import sys
+import os
+from pathlib import Path
+
+if getattr(sys, "frozen", False):
+    cwd = os.getcwd()
+    new_cwd = Path(sys.executable).parent
+    os.chdir(new_cwd)
+    if new_cwd not in sys.path:
+        sys.path.append(new_cwd)
+
 import ok
 
 from src.config import config
